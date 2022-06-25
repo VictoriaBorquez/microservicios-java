74a299691206d75033b1519bc2e12e8fdd922a16
Victoria <victoria.borquez@usach.cl>
2022-06-25 17:57:30 -0400

    Proyecto microservicios

---
+++ pep3-config-service/src/main/java/com/pep3/configservice/Pep3ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pep3.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep3ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep3ConfigServiceApplication.class, args);
+	}
+
+}
