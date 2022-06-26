254f54dfbd46d198e62d1e1d7463fc87ebbe17ce
Victoria <victoria.borquez@usach.cl>
2022-06-26 18:21:02 -0400

    Gateway

---
+++ gateway-service/src/test/java/com/pep3/configservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pep3.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
